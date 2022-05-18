443f678be40f2fec7b798caf5ee9fca60df1cfae
Irwan Gumilar <irwangumilar111@gmail.com>
2022-05-18 16:46:39 +0700

    initial commit for error handling

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPOTION: Shutting down...');
+  console.log(err.name, err.message);
+  // server.close(() => {
+  process.exit(1);
+  // });
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -11,13 +19,21 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    useNewUrlParser: true
+    // useCreateIndex: true,
+    // useFindAndModify: false
   })
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION: Shutting down...');
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
